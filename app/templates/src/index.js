c9f8cd730152f23e73c889c633d8fdfd7044ba2e
Adriaan <harianus@users.noreply.github.com>
2016-08-08 15:08:43 +0100

    Use sample function to demonstrate the use of ES6 exports

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+import sample from './utils/sample';
+
+sample();
---
+++ app/templates/src/utils/sample.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default function sample() {
+  console.log('This is the sample function running in your browser.');
+}
