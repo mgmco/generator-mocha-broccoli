a6aa5d51075c84a6b0595d6b7bf00f78890b0a37
Adriaan <harianus@users.noreply.github.com>
2016-08-10 15:49:42 +0100

    Return string in the sample utils
    So it is more easy to test

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Import sample function
 import sample from './utils/sample';
 
-sample();
+// Get message from other module
+const message = sample();
+
+// Display message in the browser
+console.log(message);
--- app/templates/src/utils/sample.js
+++ app/templates/src/utils/sample.js
@@ -1,5 +1,5 @@
 'use strict';
 
 export default function sample() {
-  console.log('This is the sample function running in your browser.');
+  return 'This is the sample function running in your browser.';
 }
